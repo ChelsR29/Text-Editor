860149d3b8d33e296910724dc972690450366399
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-06-08 01:32:36 -0400

    minor updates

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,15 +13,16 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1); 
   const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
-  const result = await request;
-  console.log('Jate saved to the database', result);
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ value: content });
+  const res = await req;
+  console.log('ID:', res, 'Jate saved to the database');
 };
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
@@ -31,7 +32,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
+  console.log('result', result);
   return result;
 };
 
